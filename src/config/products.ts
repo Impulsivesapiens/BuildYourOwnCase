b67f3960001399f5294432ccd631896c20429824
Sounak <sounakgarai180@gmail.com>
2024-07-19 01:30:26 +0530

    initial

---
+++ src/config/products.ts
@@ -0,0 +1,12 @@
+export const PRODUCT_PRICES = {
+  material: {
+    silicone: 0,
+    polycarbonate: 99_00,
+  },
+  finish: {
+    smooth: 0,
+    textured: 135_00,
+  },
+} as const
+
+export const BASE_PRICE = 199_00
