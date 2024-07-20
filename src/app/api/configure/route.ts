4b7183c1d6bbc8ad1747a0e92aaadf75b2743cb4
Subinoy Biswas <115915591+subinoybiswas@users.noreply.github.com>
2024-07-21 03:33:23 +0530

    add: delete and update in manage page

--- src/app/api/configure/route.ts
+++ src/app/api/configure/route.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from "@prisma/client";
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
 const prisma = new PrismaClient();
 
@@ -13,6 +13,7 @@ export async function GET() {
             throw new Error('Data not found');
 
         }
+        console.log(colors);
         return NextResponse.json({
             colors,
             finishes,
@@ -25,3 +26,48 @@ export async function GET() {
     }
 };
 
+export async function POST(request: NextRequest) {
+    try {
+        const { colors, finishes, materials, devices, visible } = await request.json();
+        console.log(visible);
+        if (colors && visible === 'colors') {
+            console.log(colors);
+            for (const color of colors) {
+                await prisma.color.create({
+                    data: {
+                        label: color.label,
+                        value: color.value,
+                        tw: color.tailwindColor,
+                    }
+                })
+            }
+        }
+
+        if (finishes) {
+            console.log(finishes);
+            for (const finish of finishes) {
+                await prisma.finish.create(
+                    { data: { label: finish.label, value: finish.value, description: finish.description, price: parseInt(finish.price) * 100 } })
+            }
+        }
+
+        if (materials) {
+            for (const material of materials) {
+                await prisma.material.create({ data: { label: material.label, value: material.value, description: material.description, price: parseInt(material.price) * 100 } })
+            }
+        }
+
+        if (devices) {
+            for (const device of devices) {
+                await prisma.device.create({ data: device })
+            }
+
+
+        }
+
+        return NextResponse.json({ message: 'Data updated successfully' });
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({ error });
+    }
+}
